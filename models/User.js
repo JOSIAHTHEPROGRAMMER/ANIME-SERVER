df8001d9e63cff811736aefb6e1a574b8722ebf4
JOSIAHTHEPROGRAMMER <josiahalexis15@gmail.com>
2026-03-17 23:25:03 -0400

    .

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true, trim: true },
   email: { type: String, required: true, unique: true, lowercase: true, trim: true },
   password: { type: String, required: true },
-  avatarUrl: { type: String, default: 'https://github.com/user-attachments/assets/dfb93e51-345d-4c67-9d76-40666b9f5b0a' },
+  avatarUrl: { type: String, default: '' },
   bio: { type: String, default: '' },
   following: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
   followers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
